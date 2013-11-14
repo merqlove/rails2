8e16eaa7c4cbfce1e8463ba00aff7a9f9dc5deae
Alexander Merkulov <sasha@merqlove.ru>
2013-11-14 08:19:27 +0400

    Service test

--- config/cap_tasks/nginx.rb
+++ config/cap_tasks/nginx.rb
@@ -3,15 +3,31 @@ namespace :nginx do
   task :setup do
     on roles(:web) do |host|
       template "nginx_puma.erb", "/tmp/nginx_conf"
+      template "service.erb", "/tmp/service_conf"
       on "#{fetch(:sudo_user)}@#{host}" do
-        sudo "mv /tmp/nginx_conf /etc/nginx/sites-available/#{fetch(:application)}"
-        sudo "/usr/sbin/nxensite #{fetch(:application)}"
-        sudo "rm -f /etc/nginx/conf.d/default"      
+        sudo "mv /tmp/nginx_conf /etc/nginx/sites-available/#{fetch(:application)}_#{fetch(:rails_env)}"
+        sudo "mv /tmp/service_conf /etc/init/#{fetch(:application)}_#{fetch(:rails_env)}.conf"
       end
     end
   end
 
-  %w[start stop restart].each do |command|
+  task :disable do
+    on roles(:web) do |host|
+      on "#{fetch(:sudo_user)}@#{host}" do
+        sudo "/usr/sbin/nxdissite #{fetch(:application)}_#{fetch(:rails_env)}"
+      end
+    end
+  end 
+
+  task :enable do
+    on roles(:web) do |host|
+      on "#{fetch(:sudo_user)}@#{host}" do
+        sudo "/usr/sbin/nxensite #{fetch(:application)}_#{fetch(:rails_env)}"
+      end
+    end
+  end
+
+  %w[start stop restart reload].each do |command|
     desc "#{command} nginx"
     task command do
       on roles(:web) do |host|
@@ -22,5 +38,6 @@ namespace :nginx do
     end
   end
 
-  after "nginx:setup", "nginx:restart"
+  after "nginx:setup", "nginx:enable"
+  after "nginx:enable", "nginx:reload"
 end
